b5ed1109c4414ee1b73a62070aea1f9b5c98de63
season06 <mimi871006@gmail.com>
2019-11-01 14:35:32 +0800

    HW2 fuck SRTF

--- code/threads/alarm.cc
+++ code/threads/alarm.cc
@@ -50,17 +50,19 @@ void Alarm::CallBack()
 {
     Interrupt *interrupt = kernel->interrupt;
     MachineStatus status = interrupt->getStatus();
-    // bool woken = SleepList.PutToReady();    
-	// if no program need to count, close the clock
-    // if (status == IdleMode && !woken && SleepList.Empty()) {	// is it time to quit?
+    bool woken = SleepList.PutToReady(); 
+
     kernel->currentThread->setPriority(kernel->currentThread->getPriority() - 1);
-    if (status == IdleMode) {	// is it time to quit?
+
+    if (status == IdleMode && !woken && SleepList.Empty()) {	// is it time to quit?
         if (!interrupt->AnyFutureInterrupts()) {
 	    timer->Disable();	// turn off the timer
 	}
     } else {			// there's someone to preempt
 	if(kernel->scheduler->getSchedulerType() == RR ||
-            kernel->scheduler->getSchedulerType() == Priority ) {
+            kernel->scheduler->getSchedulerType() == Priority ||
+		kernel->scheduler->getSchedulerType() == SRTF) {
+		cout << "========interrupt========" << endl;
 		interrupt->YieldOnReturn();
 	}
     }
@@ -71,6 +73,11 @@ void Alarm::WaitUntil(int x)
 	// close interrupt
 	IntStatus oldLevel = kernel->interrupt->SetLevel(IntOff);
 	Thread* t = kernel->currentThread;
+
+	// burst time
+	int worktime = kernel->stats->userTicks - t->getStartTime();
+	t->setBurstTime(t->getBurstTime() + worktime);
+	t->setStartTime(kernel->stats->userTicks);
 	cout << "Alarm::WaitUntil go sleep" << endl;
 	SleepList.PutToSleep(t, x);
 	// open interrupt
